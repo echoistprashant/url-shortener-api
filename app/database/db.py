f6d9f38672b526f689c8c6aaaabd02cdb5904f49
echoishtprashant <yadavprashant100804@gmail.com>
2026-06-13 21:42:34 +0530

    feat: migrate database to PostgreSQL

--- app/database/db.py
+++ app/database/db.py
@@ -12,11 +12,9 @@ load_dotenv()
 DATABASE_URL = os.getenv(
     "DATABASE_URL"
 )
+
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={
-        "check_same_thread": False
-    }
+    DATABASE_URL
 )
 
 SessionLocal = sessionmaker(
